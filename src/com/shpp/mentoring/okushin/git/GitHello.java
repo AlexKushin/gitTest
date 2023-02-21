da6b7f10ea14f07354d37c44a012583184e0be93
AlexKushin <kyshin1993@gmail.com>
2023-02-21 18:02:06 +0100

    second commit

--- src/com/shpp/mentoring/okushin/git/GitHello.java
+++ src/com/shpp/mentoring/okushin/git/GitHello.java
@@ -3,6 +3,8 @@ package com.shpp.mentoring.okushin.git;
 public class GitHello {
     public static void main(String[] args) {
         System.out.println("Hello!Thank you for you help!");
-        //please, write something under this comment, commit and push to my repo https://github.com/AlexKushin/gitTest.git
+        //please, clone this simple program from my repository,
+        // write something under this comment, commit and push to my repo
+        // https://github.com/AlexKushin/gitTest.git
     }
 }
