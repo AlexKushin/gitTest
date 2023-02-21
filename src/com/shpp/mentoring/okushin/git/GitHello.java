9ad452118e86e9823b6377ca34cdfba64bec6b70
AlexKushin <kyshin1993@gmail.com>
2023-02-21 17:46:45 +0100

    first commit

---
+++ src/com/shpp/mentoring/okushin/git/GitHello.java
@@ -0,0 +1,8 @@
+package com.shpp.mentoring.okushin.git;
+
+public class GitHello {
+    public static void main(String[] args) {
+        System.out.println("Hello!Thank you for you help!");
+        //please, write something under this comment, commit and push to my repo https://github.com/AlexKushin/gitTest.git
+    }
+}
