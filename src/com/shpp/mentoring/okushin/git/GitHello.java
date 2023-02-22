c369cab512366a0c15f705ede20d2dc221a8eaf0
Serhii Beseda <sergaandd@gmail.com>
2023-02-22 18:32:22 +0200

    second commit

--- src/com/shpp/mentoring/okushin/git/GitHello.java
+++ src/com/shpp/mentoring/okushin/git/GitHello.java
@@ -2,7 +2,7 @@ package com.shpp.mentoring.okushin.git;
 
 public class GitHello {
     public static void main(String[] args) {
-        System.out.println("Hello!Thank you Sasha for your help!");
+        System.out.println("Hello!Thank you Sasha for your  help!");
         //please, clone this simple program from my repository,
         // write something under this comment, commit and push to my repo
         // https://github.com/AlexKushin/gitTest.git
